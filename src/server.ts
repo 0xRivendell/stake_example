9dbb1fe157695c71bbf3498c71cf86236b7e3cfa
Shlok <shlok_mange@protonmail.com>
2024-01-22 22:16:00 +0530

    stake example

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('Hello World')
+import { Wallet } from '@ethersproject/wallet'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { config } from 'dotenv'
+import { Riv } from '@rivendell/defi'
+config()
+
+const provider = new JsonRpcProvider('https://1rpc.io/holesky')
+const wallet = new Wallet(process.env.PKEY as string, provider)
+
+const riv = new Riv('random api key')
+
+const stakeWithSdk = async () => {
+    const txObject = await riv.stake({ chainId: '17000', amount: '0.0001' })
+    const txReceipt = await wallet.sendTransaction(txObject[0])
+    console.log(txReceipt)
+}
+
+stakeWithSdk()
